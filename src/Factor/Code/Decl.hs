6d199996d37a093c2ccb25a70fa6f521d5988adf
Silvio Mayolo <mercerenies@comcast.net>
2020-04-28 21:05:22 -0400

    Fixed record desugaring

--- src/Factor/Code/Decl.hs
+++ src/Factor/Code/Decl.hs
@@ -37,20 +37,26 @@ callDrop = Call $ QId [rootAliasName, primitivesModuleName, Id "drop"] -- TODO F
 callSwap :: Statement
 callSwap = Call $ QId [rootAliasName, primitivesModuleName, Id "swap"] -- TODO Factor.Names
 
+callStar :: Statement
+callStar = Call $ QId [Id "*t"] -- TODO Factor.Names?
+
 forNthField :: Int -> Int -> Sequence
-forNthField total n = initial <> final
+forNthField total n = Sequence [
+                       Literal (FunctionValue (Function Nothing (initial <> final))),
+                       callStar
+                      ]
     where initial = fold $ replicate n (Sequence [callDrop])
           final = fold $ replicate (total - 1 - n) (Sequence [callSwap, callDrop])
 
 desugarRecord :: Id -> [Id] -> RecordInfo -> Declaration
 desugarRecord name vs (RecordInfo con fields decls) =
-    let targettype = NamedType (TypeId (QId [name]) $ fmap QuantVar vs)
+    let targettype = NamedType (TypeId (QId [name, Id "t"]) $ fmap QuantVar vs)
         fieldtypes = reverse $ fmap snd fields
         stackvar = freshVar "R" vs
         type_ = TypeDecl (Id "t") vs [TypeVal con (Stack.fromList fieldtypes)]
         fieldcount = length fields
-        accessorType t = polyFunctionType (stackvar : vs) [t] (RestQuant stackvar)
-                                                          [targettype] (RestQuant stackvar)
+        accessorType t = polyFunctionType (stackvar : vs) [targettype] (RestQuant stackvar)
+                                                          [t] (RestQuant stackvar)
         accessorFor (i, (n, t)) = FunctionDecl (accessorType t) (Function (Just n) (forNthField fieldcount i))
         accessors = fmap accessorFor $ zip [0..] fields
         inner = [type_] ++ accessors ++ decls
